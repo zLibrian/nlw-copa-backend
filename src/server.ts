d3d402ab1b302e808dbd08ac28c345bc1e7f2943
Kevin Willian <kevinwillian084@gmail.com>
2022-11-01 23:45:40 -0300

    feat: Adiciona estrutura inicial do servidor e sua primeira rota de listar "Bolãos"

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+  log: ['query']
+})
+
+const app = express();
+app.use(cors())
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/pools/count', async (req, res) => {
+  const count = await prisma.pool.count()
+  return res.status(200).json({ count })
+})
+
+app.listen(PORT, () => console.log(`Escutando na porta ${PORT}`));
+
+export default app;
